8684d8cc502783bcf6f087dd1aea2b11097fc4c2
abedel <abedelmouamine.ben-ahmed@laplateforme.io>
2024-02-03 15:41:24 +0100

    job1 modif

---
+++ Jour02/job1.py
@@ -0,0 +1,25 @@
+from dotenv import load_dotenv
+import os
+import mysql.connector
+
+load_dotenv()
+
+mydb = mysql.connector.connect(
+   
+    host = os.getenv('host'),
+    user = os.getenv('user'),
+    password = os.getenv('passwd') ,
+    database = os.getenv('database')
+)
+cursor = mydb.cursor()
+
+salle = cursor.execute("SELECT * from etudiant; ")
+
+
+results = cursor.fetchall() 
+
+    
+print(results)
+
+cursor.close()
+mydb.close()
